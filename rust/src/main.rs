21984416b74b018163408e6f623700bfefabb70a
Andrew Morris <voltrevo@gmail.com>
2019-03-15 15:22:21 +1100

    Use separate file p1.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,5 @@
+mod p1;
+
 fn main() {
-    let mut sum = 0;
-
-    for i in 1..1000 {
-        if i % 3 == 0 || i % 5 == 0 {
-            sum += i;
-        }
-    }
-
-    println!("{}", sum);
+    println!("{}", p1::calc());
 }
---
+++ rust/src/p1.rs
@@ -0,0 +1,11 @@
+pub fn calc() -> i32 {
+    let mut sum = 0;
+
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
