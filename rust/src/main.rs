bb5165a4823254adf042abfeded4cbac46a3c620
Andrew Morris <voltrevo@gmail.com>
2019-03-15 15:12:18 +1100

    write problem 1 solution directly in main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum = 0;
+
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
+        }
+    }
+
+    println!("{}", sum);
 }
